e4820202a3d0ee4184003ff39ab247f4b8ffb6c4
linkwin <alanscholar5@gmail.com>
2021-06-03 14:50:51 -0400

    Add Event System

---
+++ Assets/EventSystem/TriggerEvent.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/**
+ *
+ * NOTE: Use only 1 EventRaiser per TriggerEvent component.
+ * 
+ * @author linkwin.itch.io
+ * 
+ */
+public class TriggerEvent : MonoBehaviour
+{
+
+    public string tagFilter;
+    public bool multipleTrigger = false;
+    public bool sceneChange;
+    public string sceneToLoad;
+    private EventRaiser eventRaiser;
+    private bool hasBeenTriggered;
+
+    private void Start()
+    {
+        eventRaiser = GetComponent<EventRaiser>();
+        hasBeenTriggered = false;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == tagFilter)
+        {
+            if (multipleTrigger)
+                ManualTrigger();
+            else if (!hasBeenTriggered)
+            {
+                ManualTrigger();
+                hasBeenTriggered = true;
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("YES");
+        if (collision.gameObject.tag == tagFilter)
+        {
+            this.GetComponent<EventRaiser>().RaiseEvent();
+        }
+    }
+
+
+    public void ManualTrigger()
+    {
+        if (eventRaiser)
+            eventRaiser.RaiseEvent();
+        if (sceneChange)
+            SceneManager.LoadScene(sceneToLoad);
+    }
+}
